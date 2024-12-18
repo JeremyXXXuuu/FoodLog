d37154e9dc12adcbb8851bd599249fe55c59a102
jeremyxxxuuu <jeremyxu1234@gmail.com>
2024-12-18 11:31:46 +0100

    init project with dizzle

--- App.tsx
+++ App.tsx
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import * as SQLite from 'expo-sqlite';
+import { useEffect, useState } from 'react';
+import { drizzle } from 'drizzle-orm/expo-sqlite';
+import { usersTable } from './db/schema';
+import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
+import migrations from './drizzle/migrations';
+
+const expo = SQLite.openDatabaseSync('db.db');
+
+const db = drizzle(expo);
 
 export default function App() {
+  const { success, error } = useMigrations(db, migrations);
+  const [items, setItems] = useState<typeof usersTable.$inferSelect[] | null>(null);
+
+  useEffect(() => {
+    if (!success) return;
+
+    (async () => {
+      await db.delete(usersTable);
+
+      await db.insert(usersTable).values([
+        {
+            name: 'John',
+            age: 30,
+            email: 'john@example111.com',
+        },
+      ]);
+
+      const users = await db.select().from(usersTable);
+      setItems(users);
+    })();
+  }, [success]);
+
+  if (error) {
+    return (
+      <View>
+        <Text>Migration error: {error.message}</Text>
+      </View>
+    );
+  }
+
+  if (!success) {
+    return (
+      <View>
+        <Text>Migration is in progress...</Text>
+      </View>
+    );
+  }
+
+  if (items === null || items.length === 0) {
+    return (
+      <View>
+        <Text>Empty</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+      }}
+    >
+      {items.map((item) => (
+        <Text key={item.id}>{item.email}</Text>
+      ))}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
