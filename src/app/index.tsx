5e1ac6100d89439dda74c547e04e4a9a3460b218
jeremyxxxuuu <jeremyxu1234@gmail.com>
2024-12-19 15:52:26 +0100

    refactor: restructure app layout and migrate to new file organization

---
+++ src/app/index.tsx
@@ -0,0 +1,73 @@
+import { Text, View } from "react-native";
+import * as SQLite from "expo-sqlite";
+import { useEffect, useState } from "react";
+import { drizzle } from "drizzle-orm/expo-sqlite";
+import { usersTable } from "@/db/schema";
+import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
+import migrations from "drizzle/migrations";
+
+const expo = SQLite.openDatabaseSync("db.db");
+
+const db = drizzle(expo);
+
+export default function App() {
+  const { success, error } = useMigrations(db, migrations);
+  const [items, setItems] = useState<(typeof usersTable.$inferSelect)[] | null>(
+    null,
+  );
+  useEffect(() => {
+    if (!success) return;
+
+    (async () => {
+      await db.delete(usersTable);
+
+      await db.insert(usersTable).values([
+        {
+          name: "John",
+          age: 30,
+          email: "john@example111.com",
+        },
+      ]);
+
+      const users = await db.select().from(usersTable);
+      setItems(users);
+    })();
+  }, [success]);
+
+  if (error) {
+    return (
+      <View>
+        <Text>Migration error: {error.message}</Text>
+      </View>
+    );
+  }
+
+  if (!success) {
+    return (
+      <View>
+        <Text>Migration is in progress...</Text>
+      </View>
+    );
+  }
+
+  if (items === null || items.length === 0) {
+    return (
+      <View>
+        <Text>Empty</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View className="h-full">
+      {items.map(item => (
+        <Text
+          className="text-primary"
+          key={item.id}
+        >
+          {item.email}
+        </Text>
+      ))}
+    </View>
+  );
+}
