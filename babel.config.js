f4621fe47a317b61972ca4a08fa06cf2ee82f10c
jeremyxxxuuu <jeremyxu1234@gmail.com>
2024-12-19 15:52:19 +0100

    add nativewind integration and update project configuration

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,23 @@
 module.exports = function(api) {
     api.cache(true);
     return {
-      presets: ['babel-preset-expo'],
-      plugins: [["inline-import", { "extensions": [".sql"] }]] // <-- add this
+      presets: [
+        ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+        "nativewind/babel",
+      ],
+      plugins: [["inline-import", { "extensions": [".sql"] }],
+      ["module-resolver", {
+        root: ["."],
+        alias: {
+          "@/*": "./src/*",
+        },
+        "extensions": [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx",
+        ]
+      }]],
     };
   };
   
